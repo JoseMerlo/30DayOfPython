c0417d3bd7c12cedea8c78e8630934cd2080fea7
JoseMerlo <josemerlo027@gmail.com>
2022-02-23 23:04:20 -0600

    Dia 3

---
+++ dia_3/operadores.py
@@ -0,0 +1,48 @@
+#Booleano
+print(True)
+print(False)
+
+#Operadores aritmeticos
+#suma(+): a+b
+#resta(-): a-b
+#multiplicacion(*): a*b
+#Division(/): a/b
+#Modulo(%): a%b
+
+#Ejemplo
+print('Suma:', 1+2)
+print('Resta:', 1-2)
+print('Multiplicacion:', 2*3)
+print('Division:', 6/2)
+
+#Ejemplo flotantes
+print('Numero de punto flotante:', 2.14)
+print('Numero de punto flotante Gravedad:', 9.81)
+
+#Numeros complejos
+print('Numero complejo:', 1+1j)
+
+# Declarar la variable en la parte superior primero
+
+a  =  3  # a es un nombre de variable y 3 es un tipo de datos entero 
+b  =  2  # b es un nombre de variable y 3 es un tipo de datos entero
+
+# Operaciones aritméticas y asignación del resultado a una variable 
+total  =  a  +  b 
+diff  =  a  -  b 
+producto  =  a  *  b 
+división  =  a  /  b 
+resto  =  a  %  b 
+floor_division  =  a  //  b 
+exponencial  =  a  **  b
+
+print ( total ) # si no etiqueta su impresión con alguna cadena, nunca sabrá de dónde vendrá el resultado from 
+print( 'a + b = ' , total)
+print( 'a - b = ' , diff)
+print( 'a * b = ' , producto)
+print( 'a / b = ' , división)
+print( 'a % b = ' , resto)
+# print('a // b = ' , division_piso)
+print( 'a ** b = ' , exponencial) 
+
+#Falta mas:
